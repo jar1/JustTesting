cfc87d14195e14cebba9ac8c2feedcf37cb7b58e
JDaskevicius <j.daskevicius@gmail.com>
2011-11-15 09:52:38 +0200

    Initial commit

--- src/test/java/tests/TestCurrentTime _1Tests_F.java
+++ src/test/java/tests/TestCurrentTime _1Tests_F.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-public class TestCurrentTime
+public class TestCurrentTime _1Tests_F
 {
 	@Test
 	public void testCurrentTimeFAILED() 
--- src/test/java/tests/TestCurrentTime_3Tests_PFP.java
+++ src/test/java/tests/TestCurrentTime_3Tests_PFP.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
-public class TestCurrentTime
+public class TestCurrentTime_3Tests_PFP
 {
 	@Test
 	public void testCurrentTimeFirstTestPASSED() 
