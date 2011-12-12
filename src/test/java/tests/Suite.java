0c64f08e5ab0f2c41591f44c05402dccc5baac6e
JDaskevicius <j.daskevicius@gmail.com>
2011-12-12 17:35:32 +0200

    remove c

---
+++ src/test/java/tests/Suite.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses({
+
+		AAAAAAAAAA.class,
+		BBBBBBBBBB.class,
+
+})
+
+@RunWith(Suite.class)
+public class Suite {
+}
