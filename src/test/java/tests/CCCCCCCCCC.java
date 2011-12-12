eb93dfe7e44cb365a5821ae4a89d0a5039bf6e72
JDaskevicius <j.daskevicius@gmail.com>
2011-12-12 17:40:32 +0200

    a,b,c

---
+++ src/test/java/tests/CCCCCCCCCC.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CCCCCCCCCC
+{
+	@Test
+	public void c3() 
+	{
+		assertNotNull( System.currentTimeMillis() );		
+	}
+}
