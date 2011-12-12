9c0397721bfa76ab51b85ffb5d72648d091c5eec
JDaskevicius <j.daskevicius@gmail.com>
2011-12-12 15:34:36 +0200

    junit

--- src/test/java/tests/AAAAAAAAAA.java
+++ src/test/java/tests/AAAAAAAAAA.java
@@ -1,13 +1,13 @@
 package tests;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class AAAAAAAAAA
 {
 	@Test
 	public void AAAAAAAAAA() 
 	{
-		Assert.assertNotNull( System.currentTimeMillis() );		
+		assertNotNull( System.currentTimeMillis() );		
 	}
 }
