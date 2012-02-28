4e7323442b7cfde246db8c0033fef94ae7eb0f4b
JDaskevicius <j.daskevicius@gmail.com>
2012-02-28 13:05:19 +0200

    a

---
+++ src/test/java/tests/DDDDDDDDDD.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DDDDDDDDDD
+{
+	@Test
+	public void d1() 
+	{
+		Assert.assertNull( System.currentTimeMillis() );		
+	}
+}
